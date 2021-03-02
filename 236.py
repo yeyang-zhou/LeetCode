462a66d988584e53f33c456f96e1d3d8ea266626
yeyang-zhou <43632064+yeyang-zhou@users.noreply.github.com>
2021-03-02 21:41:49 +0800

    Create 236.py

---
+++ 236.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        l, r = self.lowestCommonAncestor(root.left, p, q), self.lowestCommonAncestor(root.right, p, q)
+        if l and r:
+            return root
+        if l:
+            return l
+        if r:
+            return r
+        return None
